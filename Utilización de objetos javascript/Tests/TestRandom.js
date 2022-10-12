9060c3f6210056bc326fbee71f9870b0dde24235
CrisPascu <104889879+CrisPascu@users.noreply.github.com>
2022-10-12 18:10:23 +0200

    Modificación tests e incluir test area

--- Utilización de objetos javascript/Tests/TestRandom.js
+++ Utilización de objetos javascript/Tests/TestRandom.js
@@ -1,28 +1,29 @@
 var expect = chai.expect;
 chai.should();
-
+//poco eficiente, investigar más
 describe("Generador de números aleatorios", function(){
     describe("Entra entre dos parámetros", function(){
         it("Tiene que ser igual o mayor que el primer parámetro", function(){
             for(let i = 0; i < 1000; i++){
-                calcularAleatorio(0, 10).should.be.at.least(0);
+                calcularAleatorio(0, 10).should.be.greaterThan(-1);
             }
             for(let i = 0; i < 1000; i++){
-                calcularAleatorio(2, 8).should.be.at.least(2);
+                calcularAleatorio(2, 8).should.be.greaterThan(1);
             }
             for(let i = 0; i < 1000; i++){
-                calcularAleatorio(5, 40).should.be.at.least(5);
+                calcularAleatorio(5, 40).should.be.greaterThan(4);
+
             }
         });
         it("Tiene que ser igual o inferior al segundo valor", function(){
             for(let i = 0; i < 1000; i++){
-                calcularAleatorio(0, 10).should.be.at.most(10);
+                calcularAleatorio(0, 10).should.not.be.at.greaterThan(10);
             }
             for(let i = 0; i < 1000; i++){
-                calcularAleatorio(2, 8).should.be.at.most(8);
+                calcularAleatorio(2, 8).should.not.be.at.greaterThan(8);
             }
             for(let i = 0; i < 1000; i++){
-                calcularAleatorio(5, 40).should.be.at.most(40);
+                calcularAleatorio(5, 40).should.not.be.at.greaterThan(40);
             }
         });
         
