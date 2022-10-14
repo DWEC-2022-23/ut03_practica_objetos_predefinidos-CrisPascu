3ce84916ca4b87bd877f97ea79002a3dfb628cea
CrisPascu <104889879+CrisPascu@users.noreply.github.com>
2022-10-14 19:45:40 +0200

    TestPalabras completo

--- Tests/TestPalabras.js
+++ Tests/TestPalabras.js
@@ -23,4 +23,18 @@ describe("Comprueba que funcionan los botones de la string", function(){
         cadenaInversa("       Comiendo comino      ").should.equal("onimoc odneimoC");
         cadenaInversa("           Sarten rota por el mango         ").should.equal("ognam le rop ator netraS");
     });
+    it("Comprueba que se divida con guiones cada caracter", function(){
+        cadenaGuiones("Hola Mundo").should.equal("H-o-l-a- -M-u-n-d-o");
+        cadenaGuiones("Hola Mundo Cruel").should.equal("H-o-l-a- -M-u-n-d-o- -C-r-u-e-l");
+        cadenaGuiones("Tres tristes tigres").should.equal("T-r-e-s- -t-r-i-s-t-e-s- -t-i-g-r-e-s");
+        cadenaGuiones("       Comiendo comino      ").should.equal("C-o-m-i-e-n-d-o- -c-o-m-i-n-o");
+        cadenaGuiones("           Sarten rota por el mango         ").should.equal("S-a-r-t-e-n- -r-o-t-a- -p-o-r- -e-l- -m-a-n-g-o");
+    });
+    it("Comprueba cuantas vocales hay en la cadena", function(){
+        contarVocales("Hola Mundo").should.equal(4);
+        contarVocales("Hola Mundo Cruel").should.equal(6);
+        contarVocales("Tres tristes tigres").should.equal(5);
+        contarVocales("       Comiendo comino      ").should.equal(7);
+        contarVocales("           Sarten rota por el mango         ").should.equal(8);
+    });
 });
