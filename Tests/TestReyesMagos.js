da0401acf4c1cbe916bb88597a580210fe9a6bf6
CrisPascu <104889879+CrisPascu@users.noreply.github.com>
2022-10-14 18:29:22 +0200

    Tests

--- Tests/TestReyesMagos.js
+++ Tests/TestReyesMagos.js
@@ -1,12 +1,26 @@
 var expect = chai.expect;
 chai.should();
 
-describe("Comprobación de fecha", function(){
+describe("Comprobación de fecha para ver cuanto falta para los reyes magos", function(){
     describe("Quedan días para la fecha", function(){
         it("Tiene que decir cuantos días quedan", function(){
-            for(let i = 0; i < 1000; i++){
-                reyesMagos(fecha).should;
-            }
+            reyesMagos(12/10/2022).should.equal(86);
+            reyesMagos(04/01/2023).should.equal(2);
+            reyesMagos(23/01/2023).should.equal(348);
+            reyesMagos(23/01/2018).should.equal(348);
+            reyesMagos(10/06/2000).should.equal(210);
+            
+        });
+        
+    });
+    describe("Es el día de los reyes magos", function(){
+        it("Tiene que decir que hoy son los reyes", function(){
+            reyesMagos(06/01/2022).should.equal(0);
+            reyesMagos(06/01/2023).should.equal(0);
+            reyesMagos(06/01/2024).should.equal(0);
+            reyesMagos(06/01/2010).should.equal(0);
+            reyesMagos(06/01/2018).should.equal(0);
+            reyesMagos(06/01/2000).should.equal(0);
         });
         
     });
